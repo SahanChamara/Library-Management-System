a0cd7626d7c550f676124889e36eef5bfd0e78d6
Sahan Chamara <sahanchamara456@gmail.com>
2025-01-18 21:31:15 +0530

    dashboard ui

--- src/main/java/edu/sc/lms/controller/dashboard/DashboardFormController.java
+++ src/main/java/edu/sc/lms/controller/dashboard/DashboardFormController.java
@@ -9,6 +9,7 @@ import javafx.geometry.Insets;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Region;
 import lombok.SneakyThrows;
 
 import java.net.URL;
@@ -32,7 +33,7 @@ public class DashboardFormController implements Initializable {
     private List<Book> getData(){
         List<Book> bookCard = new ArrayList<>();
         Book book;
-        for(int i=0; i<10; i++){
+        for(int i=0; i<20; i++){
             book = new Book();
             book.setBookTitle("Foundation");
             book.setPrice(1500.00);
@@ -47,7 +48,7 @@ public class DashboardFormController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         bookCard.addAll(getData());
         int column = 0;
-        int row = 0;
+        int row = 1;
 
         for(int i=0; i<bookCard.size(); i++){
             FXMLLoader fxmlLoader = new FXMLLoader();
@@ -57,12 +58,23 @@ public class DashboardFormController implements Initializable {
             BookCardFormController bookCardFormController = fxmlLoader.getController();
             bookCardFormController.setData(bookCard.get(i));
 
-            if(column==3){
+            if(column==5){
                 column=0;
                 row++;
             }
             grid.add(anchorPane,column++,row);
-            GridPane.setMargin(anchorPane,new Insets(10));
+
+            // setting the grid width
+            grid.setMinWidth(Region.USE_COMPUTED_SIZE);
+            grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
+            grid.setMaxWidth(Region.USE_PREF_SIZE);
+
+
+            grid.setMinHeight(Region.USE_COMPUTED_SIZE);
+            grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
+            grid.setMaxHeight(Region.USE_PREF_SIZE);
+
+            GridPane.setMargin(anchorPane,new Insets(5));
         }
     }
 }
