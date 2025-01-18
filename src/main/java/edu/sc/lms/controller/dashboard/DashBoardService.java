e03addf78fc4ef211c608b1859d91f64ba32e500
Sahan Chamara <sahanchamara456@gmail.com>
2025-01-18 15:14:38 +0530

    login some function edit

--- src/main/java/edu/sc/lms/controller/dashboard/DashBoardService.java
+++ src/main/java/edu/sc/lms/controller/dashboard/DashBoardService.java
@@ -1,5 +1,7 @@
 package edu.sc.lms.controller.dashboard;
 
+import edu.sc.lms.model.Book;
+
 public interface DashBoardService {
     boolean addBook(Book book);
 }
--- src/main/java/edu/sc/lms/controller/login/LoginFormController.java
+++ src/main/java/edu/sc/lms/controller/login/LoginFormController.java
@@ -8,6 +8,7 @@ import edu.sc.lms.model.Staff;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -15,12 +16,17 @@ import org.apache.commons.lang3.RandomStringUtils;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LoginFormController {
 
+    public JFXTextField txtCodeNum1;
+    public JFXTextField txtCodeNum2;
+    public JFXTextField txtCodeNum3;
+    public JFXTextField txtCodeNum4;
     @FXML
     private AnchorPane paneEnterOTP;
 
@@ -60,7 +66,12 @@ public class LoginFormController {
     void btnResetPassword(ActionEvent event) {
         if(txtNewResetPassword.getText().equals(txtNewConfirmPassword.getText())){
             if(LoginController.getInstance().updatePassword(txtNewResetPassword.getText(), existEmail)){
-                new Alert(Alert.AlertType.INFORMATION,"Password Reset Successful").show();
+                Optional<ButtonType> result = new Alert(Alert.AlertType.INFORMATION, "Password Reset Successful.now you can login your Dashboard.").showAndWait();
+                ButtonType buttonType = result.orElse(ButtonType.CLOSE);
+                if(buttonType==ButtonType.OK){
+                    new FadeIn(paneSignIn).play();
+                    paneSignIn.toFront();
+                }
             }else{
                 new Alert(Alert.AlertType.ERROR,"Password Reset Failed").show();
             }
@@ -85,7 +96,7 @@ public class LoginFormController {
 
     private void sendEmail(String recipentEmail) {
         String randomNumber = RandomStringUtils.randomNumeric(4);
-        generateOTP = "Your Verification Code - " + randomNumber;
+        generateOTP =randomNumber;
 
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", "true");
@@ -174,6 +185,23 @@ public class LoginFormController {
 
     @FXML
     public void btnVerifyCodeOnAction(ActionEvent actionEvent) {
+        String otpCode = txtCodeNum1.getText()+txtCodeNum2.getText()+txtCodeNum3.getText()+txtCodeNum4.getText();
+        if(otpCode.equals(generateOTP)){
+            Optional<ButtonType> result = new Alert(Alert.AlertType.INFORMATION, "Verification Code Match.Now Reset Your Password", ButtonType.OK).showAndWait();
+            ButtonType buttonType = result.orElse(ButtonType.CLOSE);
+            if(buttonType==ButtonType.OK){
+                new FadeIn(paneResetPassword).play();
+                paneResetPassword.toFront();
+            }
+        }else{
+            new Alert(Alert.AlertType.INFORMATION,"You Enter Verification is Not Match.").show();
+        }
         
+    }
+
+    public void btnResenOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnOtpEnterSectionBack(MouseEvent mouseEvent) {
     }
 }
