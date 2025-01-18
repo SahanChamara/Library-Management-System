edcd5a455c124ca14b175b0ddae9192be4e9aaec
Sahan Chamara <sahanchamara456@gmail.com>
2025-01-18 23:10:12 +0530

    dashbooard ui

--- src/main/java/edu/sc/lms/controller/dashboard/DashboardFormController.java
+++ src/main/java/edu/sc/lms/controller/dashboard/DashboardFormController.java
@@ -1,5 +1,6 @@
 package edu.sc.lms.controller.dashboard;
 
+import com.jfoenix.controls.JFXTextField;
 import edu.sc.lms.controller.bookcard.BookCardFormController;
 import edu.sc.lms.model.Book;
 import javafx.fxml.FXML;
@@ -7,11 +8,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
+import javafx.stage.Stage;
 import lombok.SneakyThrows;
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +28,31 @@ import java.util.ResourceBundle;
 public class DashboardFormController implements Initializable {
 
     @FXML
+    private TabPane TabPane;
+
+    @FXML
+    private JFXButton btnAddBookSecond;
+
+    @FXML
+    private JFXButton btnAddMemberSecond;
+
+    @FXML
     private GridPane grid;
+
+    @FXML
+    private Label lblActiveMembers;
+
+    @FXML
+    private Label lblBorrowedBooks;
+
+    @FXML
+    private Label lblDueReturns;
+
+    @FXML
+    private Label lblTotalBooks;
+
+    @FXML
+    private Label lblViewUserName;
 
     @FXML
     private ScrollPane scrollPane;
@@ -28,12 +60,53 @@ public class DashboardFormController implements Initializable {
     @FXML
     private AnchorPane tabAnchorPane;
 
-    private List<Book> bookCard = new ArrayList<>();
+    @FXML
+    private Tab tabMostPopular;
 
-    private List<Book> getData(){
+    @FXML
+    private Tab tabRecenltyAdded;
+
+    @FXML
+    private JFXTextField txtSearchBar;
+
+    private final List<Book> bookCardArray = new ArrayList<>();
+
+    private Stage dialogStage;
+
+    @FXML
+    void btnBookManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCirculationOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnMembersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReportsOnAction(ActionEvent event) {
+
+    }
+
+    private List<Book> getData() {
         List<Book> bookCard = new ArrayList<>();
         Book book;
-        for(int i=0; i<20; i++){
+        for (int i = 0; i < 20; i++) {
             book = new Book();
             book.setBookTitle("Foundation");
             book.setPrice(1500.00);
@@ -43,26 +116,24 @@ public class DashboardFormController implements Initializable {
         return bookCard;
     }
 
-    @SneakyThrows
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        bookCard.addAll(getData());
+    private void loadCard() throws IOException {
+        bookCardArray.addAll(getData());
         int column = 0;
         int row = 1;
 
-        for(int i=0; i<bookCard.size(); i++){
+        for (Book book : bookCardArray) {
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setLocation(getClass().getResource("/view/book_card__view.fxml"));
             AnchorPane anchorPane = fxmlLoader.load();
 
             BookCardFormController bookCardFormController = fxmlLoader.getController();
-            bookCardFormController.setData(bookCard.get(i));
+            bookCardFormController.setData(book);
 
-            if(column==5){
-                column=0;
+            if (column == 5) {
+                column = 0;
                 row++;
             }
-            grid.add(anchorPane,column++,row);
+            grid.add(anchorPane, column++, row);
 
             // setting the grid width
             grid.setMinWidth(Region.USE_COMPUTED_SIZE);
@@ -74,7 +145,20 @@ public class DashboardFormController implements Initializable {
             grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
             grid.setMaxHeight(Region.USE_PREF_SIZE);
 
-            GridPane.setMargin(anchorPane,new Insets(5));
+            GridPane.setMargin(anchorPane, new Insets(5));
         }
+    }
+    @SneakyThrows
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadCard();
+    }
+
+    public TabPane getTabPane() {
+        return TabPane;
+    }
+
+    public void setTabPane(TabPane tabPane) {
+        TabPane = tabPane;
     }
 }
