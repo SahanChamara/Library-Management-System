aa9d6d06d0c9e75ea0eb827fc8e21cd65dbb5de9
Sahan Chamara <sahanchamara456@gmail.com>
2025-01-28 23:19:14 +0530

    member delete transaction part

--- src/main/java/edu/sc/lms/controller/membermanage/MemberManagerController.java
+++ src/main/java/edu/sc/lms/controller/membermanage/MemberManagerController.java
@@ -1,12 +1,11 @@
 package edu.sc.lms.controller.membermanage;
 
 import com.jfoenix.controls.JFXButton;
+import edu.sc.lms.dbconnection.DBConnection;
 import edu.sc.lms.model.Member;
 import edu.sc.lms.util.CrudUtil;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -83,10 +82,30 @@ public class MemberManagerController implements MemberService{
 
     @Override
     public boolean deletemember(String memberId) {
+        Connection connection = null;
         try {
-            return CrudUtil.execute("DELETE FROM member WHERE memberId='" + memberId + "'");
+            connection = DBConnection.getInstance().getConnection();
+            connection.setAutoCommit(false);
+
+            ResultSet rst = connection.createStatement().executeQuery("SELECT recordId FROM bookrecord WHERE memberid='" + memberId + "'");
+            String recordId = rst.next()? rst.getString(1) :null;
+
+            if(connection.createStatement().executeUpdate("DELETE FROM staff_has_bookrecord WHERE BookRecord_RecordId='" + recordId + "'")>0 &&
+                    connection.createStatement().executeUpdate("DELETE FROM bookrecord WHERE memberid='" + memberId + "'")>0 && connection.createStatement().executeUpdate("DELETE FROM member WHERE memberid='"+memberId+"'")>0){
+                connection.commit();
+                return true;
+            }
+            connection.rollback();
+            return false;
         } catch (SQLException e) {
             throw new IllegalArgumentException(e);
+        }finally {
+            try {
+                assert connection != null;
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                throw new IllegalArgumentException(e);
+            }
         }
     }
 
